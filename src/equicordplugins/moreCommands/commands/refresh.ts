e662d657835d33e66728ba29e34ec5a42aadd3b0
justjxke <Jake0300@protonmai.com>
2025-09-12 02:33:13 +0100

    Functionality

--- src/equicordplugins/moreCommands/commands/refresh.ts
+++ src/equicordplugins/moreCommands/commands/refresh.ts
@@ -3,3 +3,55 @@
  * Copyright (c) 2025 Vendicated and contributors
  * SPDX-License-Identifier: GPL-3.0-or-later
  */
+
+import { ApplicationCommandInputType, sendBotMessage } from "@api/Commands";
+import { FluxDispatcher } from "@webpack/common";
+
+export default [
+    {
+        name: "refresh",
+        description: "Refreshes Discord application commands locally",
+        options: [],
+        inputType: ApplicationCommandInputType.BOT,
+        execute: async (opts, ctx) => {
+            try {
+                // Send initial feedback
+                sendBotMessage(ctx.channel.id, {
+                    content: "Refreshing application commands...",
+                });
+
+                // Dispatch the application command index fetch request
+                // This should trigger Discord to refresh its command cache
+                FluxDispatcher.dispatch({
+                    type: "APPLICATION_COMMAND_INDEX_FETCH_REQUEST",
+                });
+
+                // Also try fetching guild commands if we're in a guild
+                const guildId = ctx.guild?.id;
+                if (guildId) {
+                    FluxDispatcher.dispatch({
+                        type: "APPLICATION_COMMAND_INDEX_FETCH_REQUEST",
+                        guildId,
+                    });
+                }
+
+                // Give a small delay for the refresh to process
+                await new Promise((resolve) => setTimeout(resolve, 1000));
+
+                // Send success message
+                sendBotMessage(ctx.channel.id, {
+                    content: "Commands refreshed successfully!",
+                });
+            } catch (err) {
+                console.error(
+                    "[Refresh Command] Error refreshing commands:",
+                    err,
+                );
+                sendBotMessage(ctx.channel.id, {
+                    content:
+                        "Failed to refresh commands. Check the console for details.",
+                });
+            }
+        },
+    },
+];
--- src/equicordplugins/moreCommands/index.ts
+++ src/equicordplugins/moreCommands/index.ts
@@ -1,19 +1,7 @@
 /*
- * Vencord, a modification for Discord's desktop app
- * Copyright (c) 2022 Vendicated, Samu and contributors
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ * Vencord, a Discord client mod
+ * Copyright (c) 2025 Vendicated and contributors
+ * SPDX-License-Identifier: GPL-3.0-or-later
  */
 
 import { migratePluginSettings } from "@api/Settings";
@@ -22,10 +10,10 @@ import definePlugin from "@utils/types";
 
 import choice from "./commands/choice";
 import misc from "./commands/misc";
+import refresh from "./commands/refresh";
 import system from "./commands/system";
 import text from "./commands/text";
 import time from "./commands/time";
-import refresh from "./commands/refresh";
 
 migratePluginSettings(
     "MoreCommands",
@@ -45,6 +33,7 @@ export default definePlugin({
         EquicordDevs.ShadyGoat,
         Devs.thororen,
         Devs.Korbo,
+        EquicordDevs.justjxke,
     ],
     commands: [...choice, ...system, ...text, ...time, ...misc, ...refresh],
 });
